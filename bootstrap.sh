6c0180dbdbea40ff51ea0b08c97b900e27f98537
rgcr <rogelio.cpp@gmail.com>
2015-08-08 14:41:29 -0500

    Autoinstall vim plugins and remove .zcompdump files to ignore erros on zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,20 +6,29 @@ if ! which stow >/dev/null 2>&1; then
 fi
 
 # backup 
+echo ""
 echo "backup..."
 for f in $HOME/.zshrc* $HOME/.vimrc $HOME/.oh-my-zsh $HOME/.vim $HOME/.i3 $HOME/.tmux.conf; do
-    cp -rf $f ${f}.bak 2>/dev/null
+    mv -vf $f ${f}.bak 2>/dev/null
 done
 
 # install oh-my-zsh
+echo ""
 echo "oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 #install vundle
+echo ""
 echo "vundle..."
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
+mv -vf $HOME/.zshrc $HOME/.zshrc.orig
+
+echo ""
 echo "dotfiles..."
 for d in $(find . -maxdepth 1 -path ./.git -prune -o -type d -printf "%f " | sed 's|\.||g'); do
     stow $d
 done
+
+rm -fv $HOME/.zcompdump*
+vim +PluginInstall +qall

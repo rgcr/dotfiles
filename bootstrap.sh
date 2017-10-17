b30bcc6e5338b2b19d71a6385b4c505b5cc25b4e
rgcr <rogelio.cpp@gmail.com>
2017-10-17 15:58:22 -0500

    delete installation process, only bootstraps dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,46 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-BACKUPDIR=$HOME/.dotfiles.bak
-ZGENDIR=$HOME/.zgen/
-TPMDIR=$HOME/.tmux/plugins/tpm
 
-if ! which stow >/dev/null 2>&1; then
+_has(){
+    command type "${1}" > /dev/null 2>&1
+}
+
+
+
+BACKUPDIR=${HOME}/.dotfiles.bak
+
+if ! _has "stow" then
     echo '"stow" not found, you need to install stow'
     exit 1
 fi
 
-mkdir -p $BACKUPDIR
-echo "backup in $HOME/..."
-for f in $HOME/.zshrc* $HOME/.vim* $HOME/.oh-my-zsh $HOME/.i3 $HOME/.tmux* $HOME/.zgen*; do
-    mv -vf $f $BACKUPDIR/ 2>/dev/null
+mkdir -p ${BACKUPDIR}
+for f in ${HOME}/.zshrc* ${HOME}/.vim* ${HOME}/.i3* ${HOME}/.tmux* ${HOME}/.zplug*; do
+    mv -vf ${f} ${BACKUPDIR}/ 2>/dev/null
 done
 
-#install vim-plug
-printf "\nInstalling vim-plug"
-
-# vim
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# neovim
-mkdir -p ~/.config/nvim/
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#install zgen
-git clone https://github.com/tarjoilija/zgen.git $ZGENDIR
-
-#install tpm
-printf "\nInstalling tpm"
-git clone https://github.com/tmux-plugins/tpm $TPMDIR
+rm -fv ${HOME}/.zcompdump*
 
 # deploy dotfiles with stow
-printf "\nRestow dotfiles"
-for d in $(find . -maxdepth 1 -path ./.git -prune -o -type d -printf "%f " | sed 's|\.||g'); do
-    stow -R $d
+printf "\nRestow dotfiles\n"
+for d in $(find . -mindepth 1 -maxdepth 1 ! -path ./.git -type d -printf "%f\n"); do
+    stow -R ${d}
 done
 
-rm -fv $HOME/.zcompdump*
+cat <<_EOL_
 
-printf "Installing vim plugins"
-vim +PlugInstall +qall
+Run next command to install all vim plugins:
+  vim +PlugInstall +qall
+
+_EOL_

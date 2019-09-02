ec81a0e1bca47a1d0f77475254284faf9918ef99
rgcr <rogelio.cpp@gmail.com>
2019-09-01 23:53:51 -0500

    Improve emacs conf

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -77,11 +77,9 @@
       inhibit-startup-message t
       inhibit-startup-echo-area-message t)
 ;; ;; Font and Frame size
-(add-to-list 'default-frame-alist '(height . 40))
-(add-to-list 'default-frame-alist '(width . 90))
-;; (add-to-list 'default-frame-alist '(font . "mononoki-12"))
-;; (add-to-list 'default-frame-alist '(height . 24))
-;; (add-to-list 'default-frame-alist '(width . 80))
+(add-to-list 'default-frame-alist '(font . "Hack-9"))
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
 
 ;; ;; Turn off autosave and backup files
 (setq backup-inhibited t)
@@ -102,7 +100,6 @@
 
 ;; ;; No wrap lines
 (set-default 'truncate-lines t)
-
 ;; ;; keep the list of recent files
 (setq recentf-max-saved-items 200)
 (setq recentf-max-menu-items 200)
@@ -193,17 +190,10 @@
 ;; >>>> UI
 ;; -------------------------
 
-;; ;; Icons
-;; (use-package all-the-icons
-;;   :ensure t)
-
-;; (use-package all-the-icons-dired
-;;   :hook
-;;   (dired-mode . all-the-icons-dired-mode))
-
 ;; Highlight the current line
 (use-package hl-line
   :ensure t
+  :delight
   :config
   (global-hl-line-mode 1)
   (set-face-background hl-line-face "color-235")
@@ -211,9 +201,11 @@
 
 (use-package hl-todo
   :ensure t
+  :delight
   :init
   (setq hl-todo-highlight-punctuation ":")
-  )
+  :config
+  (global-hl-todo-mode t))
 
 ;; ;; Theme
 (use-package doom-themes
@@ -251,29 +243,44 @@
 ;; >>>> General utils
 ;; -------------------------
 
+;; Emacs packages from source
+(use-package quelpa
+  :ensure t)
+
 ;; Use double key combination
-(use-package use-package-chords
-  :ensure t
-  :config
-  (key-chord-mode 1))
+;; (use-package use-package-chords
+;;   :ensure t
+;;   :config
+;;   (key-chord-mode 1))
 
 ;; Make sure that delight is available as soon as any package triggers it.
 (use-package delight
   :commands delight)
 
+(use-package undo-tree
+  :ensure t
+  :delight
+  :init
+  (undo-tree-mode))
+
+(use-package eldoc
+  :delight
+  :config (add-hook 'prog-mode-hook 'eldoc-mode))
 
 ;; org
 (use-package org
+  :ensure t
   :mode ("\\.org\\'" . org-mode)
-  :ensure t)
+  :config
+  (setq org-cycle-separator-lines 2
+        org-deadline-warning-days 30
+        org-log-done 'time))
+
 (use-package org-bullets
   :ensure t
   :commands (org-bullets-mode)
   :init (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; Emacs package from source
-(use-package quelpa
-  :ensure t)
 
 (use-package ivy
   :ensure t
@@ -301,12 +308,14 @@
 
 (use-package swiper
   :ensure t
+  :delight
   :bind
   ([remap isearch-forward]  . swiper)
   ([remap isearch-backward] . swiper))
 
 (use-package counsel
   :ensure t
+  :delight
   :bind
   ([remap execute-extended-command] . counsel-M-x)
   :config
@@ -315,6 +324,15 @@
   (setq counsel-rg-base-command
       "rg -i -M 120 --no-heading --line-number --color never %s .")
 )
+
+(use-package ivy-rich
+  :after ivy
+  :demand t
+  :config
+  (ivy-rich-mode 1)
+  (setq ivy-virtual-abbreviate 'full
+        ivy-rich-switch-buffer-align-virtual-buffer t
+        ivy-rich-path-style 'abbrev))
 
 (use-package hydra
   :ensure t)
@@ -331,7 +349,8 @@
 
 ;; custom keybindings
 (use-package general
-  :ensure t)
+  :ensure t
+  :delight)
 
 ;; Which Key
 (use-package which-key
@@ -348,10 +367,11 @@
 
 (use-package projectile
   :ensure t
-  :delight "pj"
+  :delight "☷"
   :init
-  (setq projectile-require-project-root nil)
   (setq projectile-completion-system 'ivy)
+  (setq projectile-require-project-root nil)
+  (setq projectile-project-search-path '("~/projects/" "~/workspace/"))
   :config
   (projectile-mode t))
 
@@ -373,13 +393,17 @@
 ;;   :config
 ;;   (simpleclip-mode))
 
+;; -------------------------
+;; >>>> for os
+;; -------------------------
+(use-package daemons :ensure t)
 
 ;; -------------------------
 ;; >>>> Evil mode (vim)
 ;; -------------------------
-
 (use-package evil
   :ensure t
+  :delight "♉"
   :init
   ;; fix for <tab> in terminal (-nw)
   (setq evil-want-C-i-jump nil)
@@ -389,11 +413,12 @@
   :commands (evil-leader-mode)
   :ensure evil-leader
   :demand evil-leader
-  :init
   :config
   (progn
-    (evil-leader/set-leader ","))
-  (global-evil-leader-mode)
+    (setq evil-leader/in-all-states t)
+    (evil-leader/set-leader ",")
+    (global-evil-leader-mode)
+  )
 )
 
 (use-package evil-goggles
@@ -401,7 +426,6 @@
   :config
   (evil-goggles-mode)
   (evil-goggles-use-diff-faces))
-
 
 (use-package exec-path-from-shell
   :ensure t
@@ -426,9 +450,16 @@
   :delight
   :custom
   (company-idle-delay 0.1)
-  (company-minimum-prefix-length 2)
+  (company-minimum-prefix-length 1)
   (company-tooltip-align-annotations 't)
+  (company--show-numbers t)
   (global-company-mode t))
+
+(use-package company-quickhelp
+  :ensure t
+  :after company
+  :config
+  (company-quickhelp-mode 1))
 
 (use-package flycheck
   :ensure t
@@ -441,17 +472,18 @@
 ;; snippets
 (use-package yasnippet
   :ensure t
-  :delight yas-minor-mode "ys"
-  :diminish yas-minor-mode)
-(use-package yasnippet-snippets
-  :ensure t
-  :after yasnippet
+  :delight yas-minor-mode "ψ"
   :config
+  (add-to-list 'company-backends '(company-yasnippet))
   (yas-global-mode t))
-(use-package ivy-yasnippet
+
+(use-package yasnippet-snippets
   :ensure t
   :after yasnippet)
 
+(use-package ivy-yasnippet
+  :ensure t
+  :after yasnippet)
 
 (use-package highlight-indent-guides
   :ensure t
@@ -467,9 +499,9 @@
 (use-package magit
   :ensure t)
 
-
 (use-package smartparens
   :ensure t
+  :delight
   :hook
   (after-init . smartparens-global-mode))
 
@@ -481,6 +513,40 @@
   (dimmer-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; => LSP
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package lsp-mode
+  :hook ((python-mode php-mode) . lsp)
+  :custom
+  (lsp-prefer-flymake nil)
+  ;; (setq-default lsp-pyls-configuration-sources ["flake8"]))
+  )
+
+(use-package lsp-ui
+  :ensure t
+  :requires lsp-mode flycheck
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-position 'top
+        lsp-ui-doc-include-signature t
+        lsp-ui-sideline-enable nil
+        ;; lsp-ui-flycheck-enable t
+        ;; lsp-ui-flycheck-list-position ‘right
+        ;; lsp-ui-flycheck-live-reporting t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-list-width 60
+        lsp-ui-peek-peek-height 25
+        lsp-ui-sideline-enable nil))
+(use-package company-lsp
+  :ensure t
+  :config
+  (setq company-lsp-enable-snippet t
+        company-lsp-cache-candidates t)
+  (push 'company-lsp company-backends))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; => Langs
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -488,7 +554,7 @@
 ;; python
 (use-package python-mode
   :ensure t
-  :delight "π "
+  :delight "∫"
   :commands python-mode
   :mode ("\\.py\\'" . python-mode))
 
@@ -498,52 +564,52 @@
   :hook (python-mode . (lambda ()
                          (require 'lsp-python-ms)
                          (lsp))))
+(use-package pipenv
+  :ensure t
+  :diminish 'pipenv-mode
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setenv "WORKON_HOME" "~/.venvs/")
+  (setq pipenv-projectile-after-switch-function
+        #'pipenv-projectile-after-switch-extended))
 
-;; (use-package pipenv
-;;   :ensure t
-;;   :hook (python-mode . pipenv-mode)
-;;   :init
-;;   (setq
-;;    pipenv-projectile-after-switch-function
-;;    #'pipenv-projectile-after-switch-extended))
+(use-package json-mode
+  :ensure t
+  :mode
+  ("\\.json$"))
 
-;; (use-package elpy
-;;   :ensure t
-;;   :init
-;;   (elpy-enable))
+(use-package yaml-mode
+  :delight "γ"
+  :mode ("\\.yml$" . yaml-mode))
 
+(use-package vimrc-mode
+  :ensure t
+  :mode ("^\\.vimrc\\'"))
+
+
+(use-package ledger-mode
+  :ensure t
+  :defer t
+  :mode ("\\.ledger$"))
+
+(use-package flycheck-ledger
+  :ensure t
+  :after (flycheck ledger-mode))
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.md$"))
+
+(use-package markdown-mode+
+  :ensure t
+  :after markdown-mode
+  :defer t)
 
 ;; php
-(use-package phpactor :ensure t)
-(use-package company-phpactor :ensure t)
 (use-package php-mode
   :ensure t
-  :mode "\\.php\\'"
-  :hook ((php-mode . (lambda () (set (make-local-variable 'company-backends)
-       '(;; list of backends
-         company-phpactor
-         company-files
-         )))))
-  :init
-  (add-hook 'php-mode-hook
-          (lambda ()
-            (make-local-variable 'eldoc-documentation-function)
-            (setq eldoc-documentation-function
-                  'phpactor-hover))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; => LSP
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package lsp-mode
-  :hook ((python-mode) . lsp)
-  :custom
-  (lsp-prefer-flymake nil)
-  ;; (setq-default lsp-pyls-configuration-sources ["flake8"]))
-  )
-
-(use-package lsp-ui)
-(use-package company-lsp)
+  ;; :delight "php"
+  :mode ("\\.php\\'" . php-mode))
 
 (use-package dap-mode
   :after lsp-mode
@@ -553,26 +619,27 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; => langs
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; => Custom keybinding
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; global maps
 (global-set-key (kbd "C-s") 'swiper-isearch)
+;; create files easily in dired
+(define-key dired-mode-map (kbd "a") 'find-file)
+(put 'dired-find-alternate-file 'disabled nil) ; disables warning
+(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+(define-key dired-mode-map (kbd "DEL") (lambda () (interactive) (find-alternate-file "..")))  ; was dired-up-directory
 
 ;; global evil maps, leader => ","
 (evil-leader/set-key
   "b"  'ivy-switch-buffer            ;; Switch to buffer
   "ci" 'rgcr/comment-dwim            ;; Comment/Uncomment
-  "e"  'find-file
+  "e"  'counsel-find-file
   "i"  'highlight-indent-guides-mode
+  "k"  'kill-buffer
   "n"  'neotree-toggle               ;; Open file explorer (sidebar)
-  "p"  'counsel-yank-pop             ;; fzf - paste
+  "p"  'projectile-find-file         ;; like ctrl-p
+  "P"  'counsel-yank-pop             ;; fzf - paste
   "q"  'rgcr/kill-this-buffer        ;; Close current buffer
   "Q"  'delete-other-windows         ;; Close all other windows
   "R"  'rgcr/reload-init-file        ;; Reload emacs config
@@ -589,17 +656,20 @@
 ;; (evil-leader/set-key-for-mode 'emacs-lisp-mode
   ;; "b" 'byte-compile-file)
 
-
 ;; global general keys
 (general-define-key
  :states '(normal visual insert emacs)
  :prefix "SPC"
  :non-normal-prefix "C-SPC"
  "SPC" '(execute-extended-command :which-key "M-x")
+ "."   '(ivy-resume :which-key "Ivy Resume")
+ ;; ripgrep
  "/"   '(counsel-rg :which-key "Ripgrep")
+ ;; dired
+ "d"   '(counsel-dired :which-key "Dired")
  ;; flycheck
  "f"   '(hydra-flycheck/body :which-key "Flycheck")
- ;; Git
+ ;; maGit
  "g"   '(hydra-magit/body :which-key "Magit")
  ;; lsp
  "l"  '(hydra-lsp/body :which-key "LSP")
@@ -623,7 +693,6 @@
 (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
 (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
 
-
 ;; evil maps
 (with-eval-after-load 'evil-maps
   ;; windows
@@ -636,7 +705,6 @@
   (define-key evil-window-map "-" 'split-window-below)
   (define-key evil-window-map "|" 'split-window-right)
   ;; Ex commands
-  ;;; general
   (evil-ex-define-cmd "ls" #'ivy-switch-buffer)
   ;;; git
   (evil-ex-define-cmd "git" #'magit-status)
@@ -650,23 +718,24 @@
 ;; -------------------------
 
 ;; yasnippet
-(defhydra hydra-magit (:color blue)
+(defhydra hydra-magit (:color blue :hint nil)
   "
-^
-^Magit^             ^Do^
-^─────^─────────────^──^────────────────
-_q_ quit            _b_ blame
-^^                  _c_ clone
-^^                  _i_ init
-^^                  _s_ status
-^^                  ^^
+      Magit: %(magit-get \"remote\" \"origin\" \"url\")
+
+^Magit^
+^─────^────────────────────^
+_b_ blame     [_q_]: Cancel
+_c_ clone
+_i_ init
+_p_ pull
+_s_ status
 "
-  ("q" nil)
+  ("s" magit-status)
   ("b" magit-blame)
   ("c" magit-clone)
   ("i" magit-init)
-  ("s" magit-status))
-
+  ("p" magit-pull)
+  ("q" nil))
 
 ;; yasnippet
 (defhydra hydra-yasnippet (:color blue :hint nil)
@@ -678,7 +747,7 @@ _q_ quit            _b_ blame
  _g_lobal  _d_irectory    _i_nsert
  _m_inor   _f_ile         _t_ryout
  _e_xtra   _l_ist         _n_ew
-         _a_ll
+ ^^        _a_ll
 "
   ("d" yas-load-directory)
   ("e" yas-activate-extra-mode)
@@ -698,17 +767,14 @@ _q_ quit            _b_ blame
      :post (quit-windows-on "*Flycheck errors*")
      :hint nil)
     "
-  ^
-  ^Flycheck^          ^Do^
-  ^─────────^──────────^──^────────
-  _q_ quit             _?_ describe
-  ^^                   _d_ disable
-  ^^                   _m_ mode
-  ^^                   _f_ filter
-  ^^                   _n_ next
-  ^^                   _p_ previous
-  ^^                   _gg_ first
-  ^^                   _G_ last
+  ^Flycheck^     ^Move^
+  ^─────────^────^──^────────
+  _f_ filter     _n_  next
+  _m_ mode       _p_  previous
+  _d_ disable    _gg_ first
+  _?_ decribe    _G_  last
+
+  _q_ quit
   "
   ("?" flycheck-describe-checker)
   ("d" flycheck-disable-checker)
@@ -724,53 +790,67 @@ _q_ quit            _b_ blame
 ;; lsp
 (defhydra hydra-lsp (:exit t :hint nil)
   "
- Buffer^^               Server^^                   Symbol
--------------------------------------------------------------------------------------
- [_f_] format           [_M-r_] restart            [_d_] declaration  [_i_] implementation  [_h_] documentation
- [_m_] imenu            [_S_]   shutdown           [_D_] definition   [_t_] type            [_r_] rename
- [_x_] execute action   [_M-s_] describe session   [_R_] references   [_s_] signature"
+ Buffer^^              Symbol
+------------------------------------------------------------------------------------
+_f_ format             _d_ declaration    _i_ implementationa    _h_ documentation
+_x_ execute action     _D_ definition     _t_ type               _n_ rename
+^^                     _r_ references     _s_ signature
+^^
+LSP Actions:
+    [_R_] Restart    [_S_] Shutdown     [_I_] Session info
+"
+  ("f" lsp-format-buffer)
+  ("x" lsp-execute-code-action)
   ("d" lsp-find-declaration)
   ("D" lsp-ui-peek-find-definitions)
-  ("R" lsp-ui-peek-find-references)
+  ("r" lsp-ui-peek-find-references)
   ("i" lsp-ui-peek-find-implementation)
   ("t" lsp-find-type-definition)
   ("s" lsp-signature-help)
   ("h" lsp-describe-thing-at-point)
-  ("r" lsp-rename)
-  ("f" lsp-format-buffer)
-  ("m" lsp-ui-imenu)
-  ("x" lsp-execute-code-action)
-
-  ("M-s" lsp-describe-session)
-  ("M-r" lsp-restart-workspace)
-  ("S" lsp-shutdown-workspace))
+  ("n" lsp-rename)
+  ;; ("m" lsp-ui-imenu)
+  ("I" lsp-describe-session)
+  ("R" lsp-workspace-restart)
+  ("S" lsp-workspace-shutdown))
 
 ;; projectile
-(defhydra hydra-projectile (:color teal :hint nil
-			    :columns 4)
-  "
-PROJECTILE: %(projectile-project-root)
+(defhydra hydra-projectile (:color teal :hint nil :exit t)
 "
-  ("f"   projectile-find-file                "Find File")
-  ("r"   projectile-recentf                  "Recent Files")
-  ("z"   projectile-cache-current-file       "Cache Current File")
-  ("x"   projectile-remove-known-project     "Remove Known Project")
+         PROJECTILE: %(projectile-project-root)
 
-  ("d"   projectile-find-dir                 "Find Directory")
-  ("b"   projectile-switch-to-buffer         "Switch to Buffer")
-  ("c"   projectile-invalidate-cache         "Clear Cache")
-  ("X"   projectile-cleanup-known-projects   "Cleanup Known Projects")
+  ^Search^                ^Buffers^             ^Cache^
+  ^^───────────────────────^^───────────────────^^─────────────────────
+  _f_ Find file           _b_ Switch buffer     _z_ Cache current File
+  _p_ Fzf in projects     _k_ Kill buffers      _c_ Clear cache
+  _r_ Recent file
+  _d_ Find directory
+  _o_ Multi ocur
 
-  ("o"   projectile-multi-occur              "Multi Occur")
-  ("s"   projectile-switch-project           "Switch Project")
-  ("k"   projectile-kill-buffers             "Kill Buffers")
-  ("q"   nil "Cancel" :color blue))
+  Actions for Projects:
+      [_s_] Switch    [_a_] Add    [_x_] Remove    [_X_] Cleanup
+
+  [_q_]: Cancel
+"
+  ("s"   projectile-switch-project)
+  ("a"   projectile-add-known-project)
+  ("x"   projectile-remove-known-project)
+  ("X"   projectile-cleanup-known-projects)
+  ("f"   projectile-find-file)
+  ("p"   projectile-find-file-in-known-projects)
+  ("r"   projectile-recentf)
+  ("d"   projectile-find-dir)
+  ("o"   projectile-multi-occur)
+  ("b"   projectile-switch-to-buffer)
+  ("k"   projectile-kill-buffers)
+  ("z"   projectile-cache-current-file)
+  ("c"   projectile-invalidate-cache)
+  ("q"   nil :color red)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; => custom faces
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -790,10 +870,15 @@ PROJECTILE: %(projectile-project-root)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company--show-numbers t t)
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 1)
+ '(company-tooltip-align-annotations t)
  '(compilation-message-face (quote default))
  '(custom-safe-themes
    (quote
     ("155a5de9192c2f6d53efcc9c554892a0d87d87f99ad8cc14b330f4f4be204445" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "427fa665823299f8258d8e27c80a1481edbb8f5463a6fb2665261e9076626710" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "4e132458143b6bab453e812f03208075189deca7ad5954a4abb27d5afce10a9a" "db10381a554231a40b7474eaac28bd58f05067faacce3b25d294bb179a3511a1" "868abc288f3afe212a70d24de2e156180e97c67ca2e86ba0f2bf9a18c9672f07" default)))
+ '(global-company-mode t)
  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
  '(highlight-tail-colors
    (quote
@@ -805,10 +890,11 @@ PROJECTILE: %(projectile-project-root)
      ("#A75B00" . 70)
      ("#F309DF" . 85)
      ("#3C3D37" . 100))))
+ '(lsp-prefer-flymake nil)
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (ivy-yasnippet yasnippet-snippets php-mode switch-window org-bullets dracula-theme zenburn-theme vimrc-mode use-package-chords spaceline simpleclip rainbow-delimiters projectile popup monokai-theme molokai-theme magit ibuffer-vc highlight-indent-guides git-gutter-fringe fzf flycheck exec-path-from-shell evil-nerd-commenter evil-leader evil-goggles evil-commentary doom-themes dimmer counsel atom-one-dark-theme add-node-modules-path)))
+    (ledger-mode yaml-mode ivy-yasnippet yasnippet-snippets php-mode switch-window org-bullets dracula-theme zenburn-theme vimrc-mode use-package-chords spaceline simpleclip rainbow-delimiters projectile popup monokai-theme molokai-theme magit ibuffer-vc highlight-indent-guides git-gutter-fringe fzf flycheck exec-path-from-shell evil-nerd-commenter evil-leader evil-goggles evil-commentary doom-themes dimmer counsel atom-one-dark-theme add-node-modules-path)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(weechat-color-list
